686617936e978b093d1fad59863ee3cd518a847c
Pol Reig <polreigbroto@gmail.com>
2021-06-08 19:02:06 +0200

    HTML alimentado desde el JS

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const titulo = "Título";
+  const titular = "Titular de la noticia";
+  const bodyText =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam, iusto omnis minima corrupti est officiis natus nam,adipisci commodi quo autem magnam sequi magni odio solutatemporibus velit quas";
+  const categoriasTitulo = "Categorías";
+  const otrasCosasTitulo = "Otras cosas";
+  const categorias = [
+    "Noticias de deporte",
+    "Noticias de ocio",
+    "Noticias de politica",
+    "Noticias de espectáculos",
+  ];
+  const otrasCosas = ["Nosequé", "Talycual", "Nosecuánto"];
+  const piePagina = 50;
+  return (
+    <>
+      <div className="container">
+        <header className="cabecera text-white row align-items-center">
+          <h1 className="col m-0">{titulo}</h1>
+        </header>
+        <div className="contenedor-principal row">
+          <main className="principal col-12 col-sm-7 col-md-8 py-4">
+            <h2>{titular}</h2>
+            <p>{bodyText}</p>
+            <p>{bodyText}</p>
+          </main>
+          <aside className="sidebar col-12 col-sm-5 col-md-4 py-4">
+            <section className="listado">
+              <h3>{categoriasTitulo}</h3>
+              <ul className="list-unstyled">
+                <li>{categorias[0]}</li>
+                <li>{categorias[1]}</li>
+                <li>{categorias[2]}</li>
+                <li>{categorias[3]}</li>
+              </ul>
+            </section>
+            <section className="listado">
+              <h3>{otrasCosasTitulo}</h3>
+              <ul className="list-unstyled">
+                <li>{otrasCosas[0]}</li>
+                <li>{otrasCosas[1]}</li>
+                <li>{otrasCosas[2]}</li>
+              </ul>
+            </section>
+          </aside>
+        </div>
+        <footer className="pie text-white row align-items-center">
+          <p className="col text-center">
+            {134 > piePagina
+              ? "Texto del footer - Ningún derecho reservado, copia lo que quieras"
+              : ""}
+          </p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
